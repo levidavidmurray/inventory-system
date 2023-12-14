1c8f91bcb6136df773e5c032ddef82ca012942af
Levi Murray <levi@levimurray.dev>
2023-12-14 13:45:12 -0500

    hack for remote peer initial state

--- multiplayer/networked_hotbar.gd
+++ multiplayer/networked_hotbar.gd
@@ -2,8 +2,22 @@
 extends Hotbar
 class_name NetworkedHotbar
 
+var _selection_index_last_frame = -1
 
-func set_selection_index(new_index : int):
+
+func _process(_delta):
+	if multiplayer.is_server():
+		return
+	if is_multiplayer_authority():
+		return
+
+	# Handle initial state of remote peers
+	if _selection_index_last_frame != selection_index:
+		_selection_index_last_frame = selection_index
+		slot_changed.emit(selection_index)
+
+
+func set_selection_index(new_index: int):
 	if not multiplayer.is_server():
 		set_selection_index_rpc.rpc_id(1, new_index)
 	else:
@@ -11,12 +25,13 @@ func set_selection_index(new_index : int):
 
 
 @rpc("any_peer")
-func set_selection_index_rpc(new_index : int):
+func set_selection_index_rpc(new_index: int):
 	if not multiplayer.is_server():
 		return
 	set_selection_index_response_rpc.rpc(new_index)
-	
+
 
 @rpc("call_local")
-func set_selection_index_response_rpc(new_index : int):
+func set_selection_index_response_rpc(new_index: int):
 	super.set_selection_index(new_index)
+	_selection_index_last_frame = new_index
