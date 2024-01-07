cc1ea6ac90d82e1d13bb492a3936cafa75c2e50d
Levi Murray <levi@levimurray.dev>
2024-01-07 11:00:01 -0500

    use global position for label pos

--- core/dropped_item_3d.gd
+++ core/dropped_item_3d.gd
@@ -2,20 +2,20 @@
 extends RigidBody3D
 class_name DroppedItem3D
 
-@export var item : InventoryItem
+@export var item: InventoryItem
 @export var is_pickable := true
-@export var actions : Array[InteractAction]
-@export var default_description : String = "Get Item"
+@export var actions: Array[InteractAction]
+@export var default_description: String = "Get Item"
 
 
-func get_interaction_position(interaction_point : Vector3) -> Vector3:
-	return position
+func get_interaction_position(interaction_point: Vector3) -> Vector3:
+	return global_position
 
 
-func get_actions(_interactor : InventoryInteractor) -> Array[InteractAction]:
+func get_actions(_interactor: InventoryInteractor) -> Array[InteractAction]:
 	actions[0].description = default_description.replace("Item", item.name)
 	return actions
 
 
-func interact(interactor : InventoryInteractor, _action_index : int = 0):
+func interact(interactor: InventoryInteractor, _action_index: int = 0):
 	interactor.inventory_handler.pick_to_inventory(self)
